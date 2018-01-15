343aba720de8b11d89da9e39e930563cb48a09b4
ZZR-china <2467254599@qq.com>
2018-01-15 14:51:31 +0800

    ...

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,7 +6,19 @@ import Menus from './Menu'
 
 const SubMenu = Menu.SubMenu
 
-const Header = ({ user, logout, switchSider, siderFold, isNavbar, menuPopoverVisible, location, switchMenuPopover, navOpenKeys, changeOpenKeys, menu }) => {
+const Header = ({ 
+  user, 
+  logout,
+  switchSider, 
+  siderFold, 
+  isNavbar, 
+  menuPopoverVisible, 
+  location, 
+  switchMenuPopover, 
+  navOpenKeys, 
+  changeOpenKeys, 
+  menu
+}) => {
   let handleClickMenu = e => e.key === 'logout' && logout()
   const menusProps = {
     menu,
